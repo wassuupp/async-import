afd53efad8a66d90155dff2d71b9bd01578532f0
hitarthpattani <hitarthpattani@gmail.com>
2019-03-11 13:37:34 +0000

    Repeatable call endpoint and dummy ServiceContract

---
+++ app/code/Magento/ImportService/Model/ImportRestartOperation.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\ImportService\Model;
+
+use Magento\ImportService\Api\ImportRestartOperationInterface;
+
+/**
+ * Class ImportRestartOperation
+ *
+ * @package Magento\ImportService\Model
+ */
+class ImportRestartOperation implements ImportRestartOperationInterface
+{
+    /**
+     * restart failed operation.
+     *
+     * @param int $uuid
+     * @param string $serializedData
+     * @return bool
+     */
+    public function execute(int $uuid, string $serializedData)
+    {
+        try
+        {
+            return true;
+        } catch (\Exception $e) { }
+
+        return false;
+    }
+}
