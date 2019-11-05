2ca7d3c6966d6d8cc9610c46b10ded9340780f32
wassuupp <atihanov@comwrap.com>
2019-11-05 16:04:11 +0200

    Create Source Data Upload Endpoint (optional step) #221

---
+++ app/code/Magento/AsynchronousImportSourceDataUpload/Model/SourceDataUploadResult.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\AsynchronousImportSourceDataUpload\Model;
+
+use Magento\AsynchronousImportSourceDataUploadApi\Api\Data\SourceDataUploadResultInterface;
+
+class SourceDataUploadResult implements SourceDataUploadResultInterface
+{
+    /** @var string */
+    private $file;
+
+    /**
+     * SourceDataUploadResult constructor.
+     * @param string $file
+     */
+    public function __construct(string $file = '')
+    {
+        $this->file = $file;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getFile(): string
+    {
+        return $this->file;
+    }
+}
