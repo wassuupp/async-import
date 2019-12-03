22be8a19d21548872d7f9b8bc571f33410355642
wassuupp <atihanov@comwrap.com>
2019-12-03 14:37:20 +0200

    Advanced pricing "Add/Update" strategy #173 more changes

--- app/code/Magento/AsynchronousImportAdvancedPricing/Model/DataExchangingStrategy/AdvancedPricing.php
+++ app/code/Magento/AsynchronousImportAdvancedPricing/Model/DataExchangingStrategy/AdvancedPricing.php
@@ -9,12 +9,76 @@ namespace Magento\AsynchronousImportAdvancedPricing\Model\DataExchangingStrategy
 
 use Magento\AsynchronousImportDataExchangingApi\Api\Data\ImportInterface;
 use Magento\AsynchronousImportDataExchangingApi\Model\ExchangeDataStrategyInterface;
+use Magento\AsynchronousOperations\Model\MassSchedule;
+use Magento\Catalog\Api\Data\TierPriceInterface;
+use Magento\Catalog\Api\Data\TierPriceInterfaceFactory;
+use Magento\Catalog\Api\TierPriceStorageInterface;
+use Magento\Framework\Exception\BulkException;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Stdlib\ArrayManager;
+use Magento\Framework\Webapi\ServiceInputProcessor;
 
 /**
  * @inheritdoc
  */
 class AdvancedPricing implements ExchangeDataStrategyInterface
 {
+    public const BULK_API_TOPIC_NAME = "async.magento.catalog.api.tierpricestorageinterface.import";
+
+    /**
+     * @var TierPriceStorageInterface
+     */
+    private $priceStorage;
+
+    /**
+     * @var ServiceInputProcessor
+     */
+    private $serviceInputProcessor;
+
+    /**
+     * @var MassSchedule
+     */
+    private $massSchedule;
+
+    /**
+     * @var ArrayManager
+     */
+    private $arrayManager;
+
+    /**
+     * @var TierPriceInterfaceFactory
+     */
+    private $tierPriceFactory;
+
+    /**
+     * AdvancedPricing constructor.
+     * @param TierPriceStorageInterface $priceStorage
+     * @param ServiceInputProcessor $inputProcessor
+     * @param MassSchedule $massSchedule
+     * @param ArrayManager $arrayManager
+     * @param TierPriceInterfaceFactory $tierPriceFactory
+     */
+    public function __construct(
+        TierPriceStorageInterface $priceStorage,
+        ServiceInputProcessor $inputProcessor,
+        MassSchedule $massSchedule,
+        ArrayManager $arrayManager,
+        TierPriceInterfaceFactory $tierPriceFactory
+    ) {
+        $this->priceStorage = $priceStorage;
+        $this->serviceInputProcessor = $inputProcessor;
+        $this->massSchedule = $massSchedule;
+        $this->arrayManager = $arrayManager;
+        $this->tierPriceFactory = $tierPriceFactory;
+    }
+
+    const PRICE_KEY_NAME = 'tier_price';
+    const VALUE_KEY_NAME = 'tier_price_value_type';
+    const WEBSITE_KEY_NAME = 'tier_price_website';
+    const SKU_KEY_NAME = 'sku';
+    const CUSTOMER_GROUP_KEY_NAME = 'tier_price_customer_group';
+    const QTY_KEY_NAME = 'tier_price_qty';
+
     /**
      * @inheritdoc
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
@@ -22,6 +86,24 @@ class AdvancedPricing implements ExchangeDataStrategyInterface
      */
     public function execute(ImportInterface $import, array $importData): void
     {
+        $requestItems = [];
+        $requestItems['prices'] = [];
+        foreach ($importData as $importLine) {
+            $requestItems['prices'][] = $this->getFilledTierPriceModel($importLine);
+        }
+
+        try {
+            $this->massSchedule->publishMass(
+                self::BULK_API_TOPIC_NAME,
+                [0 => $requestItems],
+                null,
+                0
+            );
+        } catch (BulkException $e) {
+            //todo
+        } catch (LocalizedException $e) {
+            //todo
+        }
     }
 
     /**
